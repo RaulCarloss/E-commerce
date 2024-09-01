f5a5903e7efda9f50e9a8ff189f0fadd1625afc9
Raul Carlos <raulcarlos96@hotmail.com>
2024-09-01 00:12:22 -0300

    feat: add payment confirmation page

--- src/contexts/cart.context.tsx
+++ src/contexts/cart.context.tsx
@@ -18,6 +18,7 @@ interface ICartContext {
   removeProductFromCart: (productId: string) => void
   increaseProductQuantity: (productId: string) => void
   decreaseProductQuantity: (productId: string) => void
+  clearProducts: () => void
 }
 
 export const CartContext = createContext<ICartContext>({
@@ -29,7 +30,8 @@ export const CartContext = createContext<ICartContext>({
   addProductToCart: () => {},
   removeProductFromCart: () => {},
   increaseProductQuantity: () => {},
-  decreaseProductQuantity: () => {}
+  decreaseProductQuantity: () => {},
+  clearProducts: () => {}
 })
 
 const CartContextProvider: FunctionComponent = ({ children }) => {
@@ -110,6 +112,10 @@ const CartContextProvider: FunctionComponent = ({ children }) => {
     )
   }
 
+  const clearProducts = () => {
+    setProducts([])
+  }
+
   return (
     <CartContext.Provider
       value={{
@@ -121,7 +127,8 @@ const CartContextProvider: FunctionComponent = ({ children }) => {
         addProductToCart,
         removeProductFromCart,
         increaseProductQuantity,
-        decreaseProductQuantity
+        decreaseProductQuantity,
+        clearProducts
       }}
     >
       {children}
